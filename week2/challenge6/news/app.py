3120336e8563f95a174d8730ce108ec6d867fdb7
nttzl <nttzl@qq.com>
2018-08-08 13:22:35 +0800

    challenge6 pass first

--- week2/challenge6/news/app.py
+++ week2/challenge6/news/app.py
@@ -3,12 +3,34 @@ from flask import Flask,render_template,request
 import json
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-with open('files/helloshiyanlou.json','r') as f:
-    d1 = json.loads(f.read())
+def read_json(file):
+    
+    with open(file,'r') as f:
+        d = json.loads(f.read())
+    return d
 
-print(d1)
+d1 = read_json('files/helloshiyanlou.json')
+d2 = read_json('files/helloworld.json')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',d1=d1,d2=d2)
+
+@app.route('/files/<filename>')
+def file(filename):
+
+    if filename == 'helloshiyanlou':
+        result = render_template('file.html',filename=filename,d=d1)
+    elif filename == 'helloworld':
+        result = render_template('file.html',filename=filename,d=d2)
+    else:
+        result = render_template('404.html'),404
+    return result
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
+
 
