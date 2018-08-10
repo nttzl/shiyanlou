37c57cd0d8fa7ac7fa2efee141c9ce4173c7649d
nttzl <nttzl@qq.com>
2018-08-10 09:44:51 +0800

    challenge7 pass

--- week2/challenge7/news/app.py
+++ week2/challenge7/news/app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/nttzl.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/nttzl'
 db = SQLAlchemy(app)
 
 
@@ -17,13 +17,15 @@ class File(db.Model):
     title = db.Column(db.String(80))
     created_time = db.Column(db.DateTime)
 
-    category_id = db.Column(db.Integer, db.ForeignKey(category.id))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship('Category',backref=db.backref('files'))
 
     content = db.Column(db.Text)
     
-    def __init__(self,title,created_time,category,content):
+    def __init__(self,title,category,content,created_time=None):
         self.title = title
+        if created_time is None:
+            created_time = datetime.utcnow()
         self.created_time = created_time
         self.category = category
         self.content = content
@@ -42,18 +44,29 @@ class Category(db.Model):
         return '<Category %r>' % self.name
 
 
+db.drop_all()
+db.create_all()
+java = Category('Java')
+python = Category('Python')
+db.session.add_all([java,python])
+db.session.commit()
+file1 = File('Hello Java',java,'File Content - Java is cool!')
+file2 = File('Hello Python',python,'File Content - Python is cool!')
+db.session.add_all([file1,file2])
+db.session.commit()
+#    app.run(port=3000)
 
 @app.route('/')
 def index():
-    return render_template('index.html',d1=d1,d2=d2)
+    return render_template('index.html',file1=file1,file2=file2)
 
 @app.route('/files/<file_id>')
 def file(file_id):
 
-    if filename == 'helloshiyanlou':
-        result = render_template('file.html',filename=filename,d=d1)
-    elif filename == 'helloworld':
-        result = render_template('file.html',filename=filename,d=d2)
+    if int(file_id) == 1:
+        result = render_template('file.html',file=file1)
+    elif int(file_id) == 2:
+        result = render_template('file.html',file=file2)
     else:
         result = render_template('404.html'),404
     return result
@@ -61,5 +74,4 @@ def file(file_id):
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
-
 
