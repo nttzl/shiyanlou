5334039f9c656b3d9549158513d7e50e4215b458
nttzl <nttzl@qq.com>
2018-08-18 01:27:22 +0800

    challenge6 redo

--- week2/challenge6/news/app.py
+++ week2/challenge6/news/app.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
-from flask import Flask,render_template,request
-import json
+from flask import Flask,render_template,abort
+import json,os
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-def read_json(file):
-    
-    with open(file,'r') as f:
-        d = json.loads(f.read())
-    return d
-
-d1 = read_json('files/helloshiyanlou.json')
-d2 = read_json('files/helloworld.json')
+result = {}
+directory = os.path.join(os.getcwd(),'..','files')
+for i in os.listdir(directory):
+    file_path = os.path.join(directory,i)
+    with open(file_path) as f:
+            result[i[:-5]] = json.load(f)
 
 @app.route('/')
 def index():
-    return render_template('index.html',d1=d1,d2=d2)
+    l = [i['title'] for i in result.values()]
+    return render_template('index.html',l=l)
 
 @app.route('/files/<filename>')
 def file(filename):
-
-    if filename == 'helloshiyanlou':
-        result = render_template('file.html',filename=filename,d=d1)
-    elif filename == 'helloworld':
-        result = render_template('file.html',filename=filename,d=d2)
-    else:
-        result = render_template('404.html'),404
-    return result
+    f = result.get(filename)
+    if not f:
+        abort(404)
+    return render_template('file.html',f=f)
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
-
 
