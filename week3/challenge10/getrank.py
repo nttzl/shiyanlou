4d64221c3339808453974b9a7025403e9571180a
nttzl <nttzl@qq.com>
2018-08-20 02:08:21 +0800

    challenge10

--- week3/challenge10/getrank.py
+++ week3/challenge10/getrank.py
@@ -4,18 +4,25 @@ import sys
 from pymongo import MongoClient
 import pdb
 
-def get_rank(user_id):
-    client = MongoClient()
-    db = client.shiyanlou
-    contests = db.contests
+client = MongoClient()
+db = client.shiyanlou
+contests = db.contests
 
-    d = contests.find({'user_id':user_id})
-    score = 0
-    submit_time = 0
-    for i in d:
-        pdb.set_trace()
-        score += int(i['score'])
-        submit_time += int(i['submit_time'])
+dict1 = {}
+db = contests.find()
+for i in db:
+    get = dict1.get(i['user_id'])
+    if get is None:
+        dict1[i['user_id']] = {'score':int(i['score']),'time':int(i['submit_time'])}
+    else:
+        dict1[i['user_id']]['score'] = get.get('score') + int(i['score']) 
+        dict1[i['user_id']]['time'] = get.get('time') + int(i['submit_time']) 
+print(dict1.values())
+s = sorted(dict1.items(), key = lambda x: x[1])
+print(s)
+
+def get_rank(user_id):
+    
 
     return score, submit_time
 
@@ -23,9 +30,11 @@ if __name__ == '__main__':
     try:
         user_id = sys.argv[1]
         if len(sys.argv) != 2:
-            raise ValueError()
-    except ValueError:
+            raise IndexError()
+    except IndexError:
         print('Parameter Error')
         exit()
+    print
     userdata = get_rank(user_id)
     print(userdata)
+#    print(get_rank(2))
