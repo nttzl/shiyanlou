733815811cb937fe059c5b81a7f449e949391c58
nttzl <nttzl@qq.com>
2018-09-17 19:24:46 +0800

    challenge7_1 pass

--- week7/challenge7_1.py
+++ week7/challenge7_1.py
@@ -2,27 +2,36 @@ import pandas as pd
 import numpy as np
 
 def co2():
-
     data = pd.read_excel('ClimateChange.xlsx',sheetname='Data')
     country = pd.read_excel('ClimateChange.xlsx',sheetname='Country')
-    df1=pd.merge(data,country)
-    dfc = df1[df1['Series code']=='EN.ATM.CO2E.KT']
-    dfc = dfc.replace({'..':np.nan})
-    dfc = dfc.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1)
-    z=dfc.iloc[:,7:-4]
-    x = z.sum(axis=1)
-    dfc['Sum emissions']=x
-    df2 = dfc[['Income group','Country name','Sum emissions']]
-    df2=df2.replace({0:np.nan})
-    df2.dropna()
-    df3 = df2.groupby('Income group').sum()
-    df4 = df2.groupby('Income group').max()
-    df5 = df2.groupby('Income group').min()
-    df3['Highest emission country'] = df4['Country name']
-    df3['Highest emissions'] = df4['Sum emissions']
-    df3['Lowest emission country'] = df5['Country name']
-    df3['Lowest emissions'] = df5['Sum emissions']
-    return df3
+    data = data[data['Series code']=='EN.ATM.CO2E.KT'].set_index('Country code')
 
-if __name__ == '__main__':
-    print(co2())
+    data = data.drop(data.columns[0:5],axis=1)
+#dfc = pd.merge(data,country)
+    data = data.replace({'..':np.nan})
+    data = data.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1)
+    country = country.set_index('Country code')
+
+    df = pd.concat([data.sum(axis=1),country[['Income group','Country name']]],axis=1)
+    x = df.groupby('Income group').sum()
+    x.columns = ['Sum emissions']
+
+    y = df.sort_values(0,ascending=False).groupby('Income group').head(1).set_index('Income group')
+    y.columns = ['Highest emissions','Highest emission country']
+
+    z = df[df[0]>0].sort_values(0).groupby('Income group').head(1).set_index('Income group')
+    z.columns = ['Lowest emissions','Lowest emission country']
+    result = pd.concat([x,y,z],axis=1)
+    return result       
+#z=dfc.iloc[:,7:-4]
+#z = z.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1)
+#x = z.sum(axis=1)
+#dfc['Sum emissions']=x
+
+#df2 = dfc[['Income group','Country name','Sum emissions']]
+#df2=df2.replace({0:np.nan})
+#df2.dropna()
+#df3 = df2.groupby('Income group').sum()
+#df4 = df2.groupby('Income group').max()
+#df5 = df2.groupby('Income group').min()
+
